49e6f8d5ce8f9235302a056c08135d4b0f5bfeb4
Prakash <p.ghoghari1410@gmail.com>
2024-07-27 18:53:47 +0530

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_create/pages/login_page.dart';
 import 'package:flutter_create/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_create/widgets/themes.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -15,11 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
