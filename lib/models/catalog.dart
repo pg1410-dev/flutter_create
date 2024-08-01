f78d4c19483b717eead1dcaac3359cd08a16a13a
Prakash <p.ghoghari1410@gmail.com>
2024-08-01 16:42:42 +0530

    Day10 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -4,13 +4,15 @@ class Item {
   final String desc;
   final num price;
   final String color;
+  final String image;
 
   Item(
       {required this.id,
       required this.name,
       required this.desc,
       required this.price,
-      required this.color});
+      required this.color,
+      required this.image});
 }
 
 final products = [
@@ -20,5 +22,7 @@ final products = [
     desc: "Apple iPhone 12th generation",
     price: 999,
     color: "#33505a",
-  )
+    image:
+        "https://5.imimg.com/data5/SELLER/Default/2021/4/JX/FC/BN/127651858/apple-iphone-12-pro-gold-png-1000x1000.png",
+  ),
 ];
